3a901858700a7b5580d02ba944e6f6fe779958fa
deepanm87 <65060511+deepanm87@users.noreply.github.com>
2025-08-12 20:19:22 -0400

    updated tab title and tab icon and pokemon data api

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-export default function Home() {
+import { getPokemonData } from "@/utils/getPokemonData"
+
+export default async function Home() {
+  const pokemons = await getPokemonData()
+
   return (
     <div className="min-h-screen bg-gradient-to-b from-red-500 to-red-600">
       <div className="container mx-auto px-4 py-8">
